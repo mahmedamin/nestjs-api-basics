cf385ad5ca06079be493c07e37f6b9bd1c4fb43a
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2023-05-21 21:46:16 +0500

    Add request body to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import {Body, Controller, Post, Req} from '@nestjs/common';
 import { AuthService } from './auth.service';
+// import {Request} from "express";
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() body: any) {
+    console.log('-req1', body);
     return this.authService.signup();
   }
 
